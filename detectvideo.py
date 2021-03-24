b0e8a335b1aa22dced631b923b3a5e80d9697d2c
ProtossDragoon <dlwkdgn1@naver.com>
2021-03-24 13:02:49 +0900

    bugfix (https://github.com/opencv/opencv/issues/14884) and edit gitignore

--- detectvideo.py
+++ detectvideo.py
@@ -1,3 +1,4 @@
+import cv2
 import time
 import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
@@ -9,7 +10,6 @@ import core.utils as utils
 from core.yolov4 import filter_boxes
 from tensorflow.python.saved_model import tag_constants
 from PIL import Image
-import cv2
 import numpy as np
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
